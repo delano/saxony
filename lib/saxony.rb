30ddc954aa4da3e08cda53246f65684203367eb1
delano <delano@solutious.com>
2010-01-31 22:35:11 -0500

    First pass

---
+++ lib/saxony.rb
@@ -0,0 +1,89 @@
+require 'nokogiri'
+require 'stringio'
+
+class Saxony 
+  class Document < Nokogiri::XML::SAX::Document
+    attr_reader :total_count, :granularity
+    def initialize(element, granularity, &processor)
+      @element, @processor = element, processor
+      @granularity, @total_count = granularity, 0
+      reset
+    end
+    def start_element(element, attributes)
+      if element == @element.to_s
+        @count += 1 and @total_count += 1
+        @collect = true 
+      end
+      @xml << to_otag(element, attributes) if @collect
+    end
+    def characters(text)
+      @xml << text if @collect
+    end
+    def cdata_block(text)
+      @xml << to_cdata(text) if @collect
+    end
+    def end_element(element)
+      @xml << to_ctag(element) if @collect
+      if element == @element.to_s
+        @collect = false
+        @xml << $/
+        process_objects if @count >= @granularity
+      end
+    end
+    def end_document
+      process_objects unless @xml.empty?
+    end
+    def process_objects
+      @xml.rewind
+      @xml = "<SAXONYDOC>#{@xml.read}</SAXONYDOC>"
+      self.instance_eval &@processor
+      reset
+    end
+    private
+    def doc
+      @doc ||= Nokogiri::XML(@xml)
+    end
+    def reset
+      @xml, @count, @doc = StringIO.new, 0, nil
+    end
+    def to_otag(name, attributes=[])
+      t = name
+      "<#{t}>"
+    end
+    def to_ctag(name)
+      "</#{name}>"
+    end
+    def to_cdata(text)
+      "<![CDATA[#{text}]]>"
+    end
+  end
+  
+  attr_reader :granularity, :element
+  def initialize(element, granularity=1000)
+    @element, @granularity = element, granularity
+  end
+    
+  def parse *sources, &blk
+    sources.flatten!
+    sources.each do |src|
+      saxdoc = Saxony::Document.new @element, @granularity, &blk
+      parser = Nokogiri::XML::SAX::Parser.new(saxdoc)
+      xml = (String === src && File.exists?(src)) ? File.open(src) : src
+      parser.parse xml
+    end
+  end
+end
+
+#STDERR.print '.' if @samples % 5000 == 0
+
+if $0 == __FILE__
+  sax = Saxony.new :Listing, 10000
+  start = Time.now
+  sax.parse ARGV do
+    elapsed = Time.now - start
+    p [total_count, doc.xpath("//Listing").size, elapsed.to_f]
+    start = Time.now
+#    p 
+  end
+end
+
